f1068bf894baa5403cec6f0c0d31ee6b12dc5dc0
sunieee <1287495769@qq.com>
2023-03-19 23:16:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# discord开发
+
+关键的概念是user和session，对应着用户和会话
+
+- 首先discord中每个用户（user）对应chatGPT的用户，相应数据存储在user数据表中
+- 每个user可以创建多个会话（session），并且可以对于创建者是自己的会话进行管理
+- 用户可使用自己的会话或者公共会话进行对话
+  - 如果在公共channel创建session，那么session初始化为公共的（任何人都能进行提问）
+  - 如果在DM创建的session，那么session初始化为私有的，即使在公共对话，其他人也无法提问（除非创建者修改权限）
+
+## schema
+
+使用sqlite存储信息到本地，包含session和user两个表格：
+
+**user表：**包括以下信息：name + discod_user_id + 时间（create, update指最晚对话时间, modify指最晚信息修改时间）+ status + model + current_session + system_msg
+
+**session表：**包括以下信息：创建者 + 模型 + 标题 + 时间（create, update指最晚对话时间, modify指最晚信息修改时间）+ 是否公开 + 序列化对话数据（保存每句话的身份）+ system_msg
+
+## 斜杠命令
+
+使用相应的斜杠命令会私发一个消息，该交互具有三个重要的特征：输入参数、输出内容、按钮。
+
+- 输入参数：在命令后面跟上的参数，用于修改字符型属性
+- 输出内容：有关于该数据结构的全部信息输出
+- 按钮：消息的末尾是按钮，可以对于数据结构的属性进行调整
+
+### /user
+
+输入参数：
+
+- system_msg：默认会话设定（仅在gpt3.5以上模型中有效）
+- max_token：最大单次询问token数量（默认为500）
+
+输出内容：
+
+- Name (Discord id)
+- Create time
+- Update time
+- Modify time
+- System message
+- Sessions
+
+按钮：
+
+- **Show public**：是否显示公共对话，否则只显示自己创建的，从是切换到否，或者从否切换到是（默认为False）
+- **attach**：创建会话后是否默认进入会话上下文，否则用户状态会始终处于new（默认为False）
+- **clear**：将已有的会话全部清除，慎用
+- **使用模型（row2）**：仅对新创建的会话有效，已有会话模型不会变动，在请求API时使用相应的模型
+- **用户状态（row3）**：用户三种状态：new, activate, disable，分别代表“创建新会话”、“使用已有会话”、“禁用ChatGPT”，默认处于“禁用ChatGPT”状态。前两种状态均是问答状态，该用户所有消息的都会得到回复。
+- **使用会话（row4）**：列举所有会话，可选择已有会话（个人或者共享的），点击已有会话后私发会话栈信息，并且后续的对话有栈的环境。将状态改为new会创建新的会话，默认使用问句中的关键词进行会话命名
+
+### /session
+
+输入参数：
+
+- name：重命名当前会话（默认创建的前10个字符）
+- system_msg：会话设定
+
+输出内容：
+
+- Creator
+- Name
+- Create time
+- Update time
+- Modify time
+- System message
+- Conversation
+
+按钮：
+
+- **public**：会话是否是公共的，只有会话创建者才能修改
+- **model**：会话使用的模型（只对该session后续的对话有效）
+- **remove**：移除当前会话
+
+## 可选模型
+
+OpenAI API 由具有不同功能和价位的多种模型提供支持。[您还可以通过微调](https://platform.openai.com/docs/guides/fine-tuning)针对您的特定用例对我们的原始基础模型进行有限的定制。
+
+| 模型种类                                                     | 描述                                                      |
+| ------------------------------------------------------------ | --------------------------------------------------------- |
+| [GPT-4](https://platform.openai.com/docs/models/gpt-4)       | 一组在 GPT-3.5 上改进的模型，可以理解并生成自然语言或代码 |
+| [GPT-3.5](https://platform.openai.com/docs/models/gpt-3-5)   | 一组在 GPT-3 上改进的模型，可以理解并生成自然语言或代码   |
+| DALL E                                                       | 可以在给定自然语言提示的情况下生成和编辑图像的模型        |
+| [W](https://platform.openai.com/docs/models/whisper)hisper   | 一种可以将音频转换为文本的模型                            |
+| [E](https://platform.openai.com/docs/models/embeddings)mbedding | 一组可以将文本转换为数字形式的模型                        |
+| CodeX                                                        | 一组可以理解和生成代码的模型，包括将自然语言翻译成代码    |
+| [M](https://platform.openai.com/docs/models/moderation)oderation | 可以检测文本是否敏感或不安全的微调模型                    |
+| [GPT-3](https://platform.openai.com/docs/models/gpt-3)       | 一组可以理解和生成自然语言的模型                          |
+
+代表模型：gpt-4、[gpt-3.5-turbo](https://platform.openai.com/docs/models)、text-davinci-003
+
+虽然新`gpt-3.5-turbo`模型针对聊天进行了优化，但它非常适合传统的完成任务。原始的 GPT-3.5 模型针对[文本补全](https://platform.openai.com/docs/guides/completion)进行了优化。还发布了开源模型，包括[Point-E](https://github.com/openai/point-e)、[Whisper](https://github.com/openai/whisper)、[Jukebox](https://github.com/openai/jukebox)和[CLIP](https://github.com/openai/CLIP)。
+
+价格：https://openai.com/pricing
+
+![img](https://act-visual.feishu.cn/space/api/box/stream/download/asynccode/?code=NTI3OTVkY2UwYWQyZTczMmY5NTUyNjQyNTg3MDEwNjNfRWwzNkViYVFqRk9QT1NhVFNJM2ZobFZEbVNmZEs5c2hfVG9rZW46Ym94Y25tN3owVzN2Qml2OXdJWjdkYzNhY1ZiXzE2NzkyMzg2NzM6MTY3OTI0MjI3M19WNA)
+
+chatAPI：https://platform.openai.com/docs/guides/chat/introduction
+
+## 消息命令
+
+为了避免遗忘历史信息，提供对机器人消息的消息命令，私发有关session的所有环境信息
+
+> Plus账号信息：
+>
+> liubx07@gmail.com sensetime
+>
+> API key: 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
+>
+> Organization id: org-LQ0zTSTQKSxRHoDfiU3cLmtw
+>
+> I'd like to build a discord chat bot, especially integrate it with "Visual ChatGPT", which can help us interpret photos or generate photos highly interactively.
+>
+> 账号：latavislgvjuyp@gmail.com
+>
+> 密码：SSss77&&
+>
+> api_key：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
+>
+> Org ID: org-MrioZE7Irj32h0rnPjrFxAQa
+>
+> Discord token: MTA4NzAyNDE5OTYyMjcyMTcyOA.GdxKj5.PdcAnH7mLAzzTO27uWhqNyPcLIiXwoq6ibSiRg
+
+
+
+![img](https://act-visual.feishu.cn/space/api/box/stream/download/asynccode/?code=N2ZhMjk1ZTE0OGQzYzFkNmU4ZTJkZGVjMjM1NDU4OTVfVVdIdnUwNDhmUXE2aXVjc0JPdk11T2pEd1BFM05rT3hfVG9rZW46Ym94Y25sb0pLeTgxSVRUaDd4M2J0WWtCTjllXzE2NzkyMzg2NzM6MTY3OTI0MjI3M19WNA)
---
+++ discord-chat.py
@@ -0,0 +1,385 @@
+import os
+import discord
+from discord import app_commands
+from discord.ui import Button, View
+import asyncio
+import pandas as pd
+import openai
+import requests_async as req 
+from asgiref.sync import sync_to_async
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Boolean, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+import json
+from datetime import datetime
+import numpy as np
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+default_model = "gpt-3.5-turbo"
+failed_str = '[Failed]'
+chat_models = ['gpt-4', 'gpt-3.5-turbo']
+models = ['gpt-4', 'gpt-3.5-turbo', 'text-davinci-003']
+
+def init_client(proxy):
+    intends = discord.Intents.default()
+    intends.members = True
+    intends.presences = True
+    intends.messages = True
+    intends.message_content = True
+    return discord.Client(intents=intends, proxy=proxy if proxy else None)
+
+client = init_client(os.getenv('http_proxy'))
+tree = app_commands.CommandTree(client)
+MY_GUILD = None
+
+# 启动mysql连接，到本地sqlite数据库
+engine = create_engine('sqlite:///chat.db?check_same_thread=False', echo=True)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    # 默认是discord_user_id
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String)
+    create_time = Column(DateTime, default=datetime.now())
+    update_time = Column(DateTime, default=datetime.now())
+    modify_time = Column(DateTime, default=datetime.now())
+    status = Column(String)
+    model = Column(String)
+    current_session_id = Column(Integer)
+    sessions = relationship("Session", backref="creator")
+    system_msg = Column(String)
+    show_public = Column(Boolean)
+    attach = Column(Boolean)
+    max_tokens = Column(Integer)
+    temperature = Column(Float)
+
+    def strfy(self):
+        s = f'''name: {self.name} (id: {self.id})
+max tokens: {self.max_tokens}
+temperature: {self.temperature}
+create time: {self.create_time.strftime('%m/%d/%Y, %H:%M:%S')}
+update time: {self.update_time.strftime('%m/%d/%Y, %H:%M:%S')}
+modify time: {self.modify_time.strftime('%m/%d/%Y, %H:%M:%S')}
+system message: {self.system_msg if self.system_msg else '(None)'}
+owned sessions: {'' if len(self.sessions) else '(None)'}
+'''
+        for session in self.sessions:
+            s += '\t{:>10s}: {}\n'.format(str(session.id), session.name)
+        return s
+
+
+class Session(Base):
+    __tablename__ = 'session'
+    id = Column(Integer, primary_key=True)
+    creator_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(String)
+    create_time = Column(DateTime, default=datetime.now())
+    update_time = Column(DateTime, default=datetime.now())
+    modify_time = Column(DateTime, default=datetime.now())
+    model = Column(String)
+    public = Column(Boolean)
+    conversation = Column(String)
+    system_msg = Column(String)
+
+    def strfy(self):
+        s = f'''name: {self.name}
+creator: {self.creator.name} (id:{self.creator_id})
+create time: {self.create_time.strftime('%m/%d/%Y, %H:%M:%S')}
+update time: {self.update_time.strftime('%m/%d/%Y, %H:%M:%S')}
+modify time: {self.modify_time.strftime('%m/%d/%Y, %H:%M:%S')}
+system message: {self.system_msg if self.system_msg else '(None)'}
+conversation:
+'''
+        for item in json.loads(self.conversation):
+            s += '\t{:>10s}: {}\n'.format(item[0], item[1])
+        return s
+    
+    def request(self, prompt, username):
+        conversation = json.loads(self.conversation)
+        conversation.append((username, prompt))
+        print(conversation)
+        try:
+            if self.model in chat_models:
+                messages = [{'role': 'assistant' if msg[0] == 'assistant' else 'user', 'content': msg[1]} for msg in conversation]
+                if self.system_msg:
+                    messages = [{"role": "system", "content": self.system_msg}] + messages
+                response = openai.ChatCompletion.create(
+                    model=self.model,
+                    messages = messages,
+                    temperature=self.creator.temperature,
+                    max_tokens=self.creator.max_tokens,
+                    top_p=1,
+                )
+                tokens = response['usage']['total_tokens']
+                ret = response['choices'][0]['message']['content'].replace('\n\n', '\n')
+                print(tokens)
+            else:
+                s = ''
+                for item in conversation:
+                    if item[0] in ['AI', 'assistant']:
+                        s += f'assistant: {item[1]}\n'
+                    else:
+                        s += f'user: {item[1]}\n'
+                response = openai.Completion.create(
+                    model=self.model,
+                    prompt=s,
+                    temperature=self.creator.temperature,
+                    max_tokens=self.creator.max_tokens,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0.6,
+                    stop=["user:", "assistant:"]
+                )
+                ret = response.choices[0].text
+            # print(ret)
+        except Exception as e:
+            return failed_str + str(e)
+
+        conversation.append(('assistant', ret))
+        self.conversation = json.dumps(conversation)
+        self.update_time = datetime.now()
+        self.creator.update_time = datetime.now()
+        db_session.commit()
+        return ret.replace('AI: ', '').replace('AI Assistant: ', '')
+
+    async def async_request(self, prompts, username):
+        try:
+            ret = await asyncio.wait_for(sync_to_async(self.request)(prompts, username), 60)
+        except Exception as e:
+            ret = failed_str + str(e)
+        return ret
+
+
+Base.metadata.create_all(engine)
+db_session = sessionmaker(bind=engine)()
+    
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=MY_GUILD)
+    print("Ready!")
+
+
+def get_user(author):
+    user = db_session.query(User).filter_by(id=author.id).first()
+    if not user:
+        user = User(id=author.id, name=author.name, status='disable', model=default_model, 
+                    current_session_id=0, system_msg='', show_public=False, attach=False,
+                    max_tokens=600, temperature=0.8)
+        db_session.add(user)
+        db_session.commit()
+
+    if user.name != author.name:
+        user.name = author.name
+        db_session.commit()
+    return user
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author.bot:
+        return
+    
+    user = get_user(message.author)
+    if user.status == 'disable':
+        return
+
+    if user.status == 'new':
+        is_private = isinstance(message.channel, discord.channel.DMChannel)
+        session = Session(creator=user, name=message.content[:10], model=user.model, 
+                          public=not is_private, conversation='[]', system_msg=user.system_msg)
+    else:
+        session = db_session.query(Session).filter_by(id=user.current_session_id)
+
+    if session.creator != user and not session.public:
+        await message.reply(f'{failed_str}This is a private session created by {session.creator.name}.')
+        return
+
+    await message.add_reaction('✍🏻')
+    ret = await session.async_request(message.content, user.name)
+    await message.reply(ret)
+    await message.remove_reaction('✍🏻', client.user)
+    await message.add_reaction('👌')
+
+
+async def get_session(interaction: discord.Interaction):
+    await interaction.response.defer()
+    user = get_user(interaction.user)
+    if user.status != 'activate':
+        await interaction.followup.send(failed_str + 'You are not activating any session!')
+        return
+    
+    session = db_session.query(Session).filter_by(id=user.current_session_id)
+    if session.creator != user:
+        await interaction.followup.send(f'{failed_str}This is a private session created by {session.creator.name}.')
+        return
+    return session
+
+
+@tree.command(name = "session", description = "reset current session", guild=MY_GUILD) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def set_session(interaction: discord.Interaction, name:str='', system_msg:str=''):
+    session = await get_session(interaction)
+    if not session:
+        return
+    if name or system_msg:
+        session.name = name
+        session.system_msg = system_msg
+        session.modify_time = datetime.now()
+        db_session.commit()
+    await interaction.followup.send(session.strfy(), view=SessionView(session))
+
+
+class SettingButton(Button):
+    def __init__(self, name, activated, row=None, id=None, disabled=False):
+        super().__init__(label=name, style=discord.ButtonStyle.blurple if activated else discord.ButtonStyle.gray, 
+                         row=row, disabled=disabled)
+        self.id = id
+        self.name = name
+
+    async def callback(self, interaction: discord.Interaction):
+        if self.style == discord.ButtonStyle.blurple:
+            return
+        await self._view.change_model(interaction, self)
+
+
+class BoolButton(Button):
+    def __init__(self, name, activated, row=None, disabled=False):
+        super().__init__(label=name, style=discord.ButtonStyle.green if activated else discord.ButtonStyle.gray, 
+                         row=row, disabled=disabled)
+        self.name = name
+        self.activated = activated
+
+    async def callback(self, interaction: discord.Interaction):
+        self.activated = not self.activated
+        self.style = discord.ButtonStyle.green if self.activated else discord.ButtonStyle.gray
+        await self._view.change_bool(interaction, self)
+
+
+class SessionView(View):
+    def __init__(self, session):
+        super().__init__(timeout=None)
+        self.session = session
+        user = session.creator
+        self.model_buttons = []
+
+        button = Button(label='remove', style=discord.ButtonStyle.red)
+        async def delete_session(interaction: discord.Interaction):
+            db_session.delete(session)
+            user.status = 'new'
+            db_session.commit()
+            await interaction.followup.send(f'Successfully delete session: {session.name} (id: {session.id}))')
+        button.callback = delete_session
+        self.add_item(button)
+        self.add_item(BoolButton('public', session.public))
+
+        for model in models:
+            button = SettingButton(model, session.model == model, row=2)
+            self.add_item(button)
+            self.model_buttons.append(button)
+
+    async def change_bool(self, interaction: discord.Interaction, button):
+        self.session.public = not self.session.public
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(f'Successfully change {button.name} to `{button.activated}`', ephemeral=True)
+
+    async def change_model(self, interaction: discord.Interaction, button):
+        self.session.model = button.name
+        db_session.commit()
+        for b in self.model_buttons:
+            b.style = discord.ButtonStyle.gray
+        button.style = discord.ButtonStyle.blurple
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(f'Successfully change model to `{button.name}`', ephemeral=True)
+
+
+class UserView(View):
+    def __init__(self, user):
+        super().__init__(timeout=None)
+        self.user = user
+        self.status_buttons = []
+        self.model_buttons = []
+
+        button = Button(label='clear', style=discord.ButtonStyle.red)
+        async def clear_session(interaction: discord.Interaction):
+            for session in user.sessions:
+                db_session.delete(session)
+            db_session.commit()
+            await interaction.response.send_message('All sessions have been cleared!', ephemeral=True)
+        button.callback = clear_session
+        self.add_item(button)
+        self.add_item(BoolButton('show public', user.show_public))
+        self.add_item(BoolButton('attach', user.attach))
+
+        for model in models:
+            button = SettingButton(model, user.model == model, row=2)
+            self.add_item(button)
+            self.model_buttons.append(button)
+        
+        for status in ['new', 'disable', 'activate']:
+            button = SettingButton(status, user.status == status, row=3, disabled=status=='activate')
+            self.add_item(button)
+            self.status_buttons.append(button)
+
+        for session in user.sessions:
+            button = SettingButton(session.name, user.current_session_id == session.id, id=session.id, row=4)
+            self.add_item(button)
+            self.status_buttons.append(button)
+
+        if user.show_public:
+            for session in db_session.query(Session).filter_by(public=True):
+                if session.creator != user:
+                    button = SettingButton(session.name, user.current_session_id == session.id, id=session.id, row=4)
+                    self.add_item(button)
+                    self.status_buttons.append(button)
+    
+
+    async def change_bool(self, interaction: discord.Interaction, button):
+        if button.name == 'show public':
+            self.user.show_public = not self.user.show_public
+        elif button.name == 'attach':
+            self.user.attach = not self.user.attach
+        db_session.commit()
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(f'Successfully change {button.name} to `{button.activated}`', ephemeral=True)
+
+
+    async def change_model(self, interaction: discord.Interaction, button):
+        if button in self.status_buttons:
+            buttons = self.status_buttons
+            if button.name in ['new', 'disable']:
+                self.user.status = button.name
+                self.user.current_session_id = 0
+                msg = f'successfully change status to {button.name}'
+            else:
+                self.user.status = 'activate'
+                self.user.current_session_id = button.id
+                session = db_session.query(Session).filter_by(id=button.id)
+                msg = 'successfully change session!\n' + session.strfy()
+            
+        else:
+            buttons = self.model_buttons
+            self.user.model = button.name
+            msg = f'successfully change model to {button.name}'
+        
+        db_session.commit()
+        for b in buttons:
+            b.style = discord.ButtonStyle.gray
+        button.style = discord.ButtonStyle.blurple
+        self.activate_button = discord.ButtonStyle.blurple if self.user.status == 'activate' else discord.ButtonStyle.gray
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(msg, ephemeral=True)
+
+
+@tree.command(name = "user", description = "list sessions of ChatGPT", guild=MY_GUILD) 
+async def set_user(interaction: discord.Interaction, system_msg: str=''):
+    user = get_user(interaction.user)
+    if system_msg and system_msg != user.system_msg:
+        user.system_msg = system_msg
+        user.modify_time = datetime.now()
+        db_session.commit()
+    await interaction.response.send_message(user.strfy(), view=UserView(user), ephemeral=True)
+
+
+client.run(os.getenv("DISCORD_TOKEN"))
